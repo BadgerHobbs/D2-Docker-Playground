1113cb6803911afb5b9ae9019a8c593ec7952144
BadgerHobbs <andyjriggs@gmail.com>
2022-11-25 14:23:12 +0000

    Init

---
+++ api.py
@@ -0,0 +1,62 @@
+import os
+import uuid
+import subprocess
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+    return send_from_directory("static", "index.html")
+
+@app.route("/api/generate", methods=["POST"])
+def generate():
+
+    # Get request data
+    data = request.get_json()
+
+    # Get d2 diagram data
+    diagram_content = data.get("diagram")
+
+    file_name = None
+    for i in range(0,100):
+
+        file_name = f"/tmp/{uuid.uuid4()}"
+
+        if not os.path.exists(f"{file_name}.d2"):
+            break
+
+    if not file_name:
+        return
+            
+    # Create file content
+    f = open(f"{file_name}.d2", "w")
+    f.write(diagram_content)
+    f.close()
+
+    process = subprocess.run([
+        "d2",
+        f"{file_name}.d2",
+        f"{file_name}.svg"
+    ])
+
+    # Get svg file content
+    f = open(f"{file_name}.svg", "r")
+    diagram_svg = f.read()
+    f.close()
+
+    os.remove(f"{file_name}.d2")
+    os.remove(f"{file_name}.svg")
+
+    return jsonify({
+        "diagram": diagram_svg
+    })
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0", 
+        port=5000,
+        debug=True,
+    )
---
+++ dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update \
+  && apt-get install -y python3-pip python3-dev \
+  && cd /usr/local/bin \
+  && ln -s /usr/bin/python3 python \
+  && pip3 install --upgrade pip \
+  && curl -fsSL https://d2lang.com/install.sh | sh -s --
+
+COPY . /app
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+CMD python3 -u ./api.py
